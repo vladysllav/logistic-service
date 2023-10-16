080f615d569427fa62b8cd081388f03510e4f49b
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-10-16 16:49:42 +0300

    fix pre -commit

--- src/config/urls.py
+++ src/config/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
-# daskdlsandhascvpqwebvwep'vbwe'vpwe  bv  pwevewnlomfwek;f,wefwefwefef32222222222222222222222222222222222222222222222222222222222222222222
