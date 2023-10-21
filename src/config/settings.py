45f27a3205bbcf109ed411d0a981bd20f1fb5e0b
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-10-21 11:51:24 +0300

    Add custom User Model

--- src/config/settings.py
+++ src/config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -151,3 +153,5 @@ CACHES = {
         }
     }
 }
+
+AUTH_USER_MODEL = "users.CustomUser"
