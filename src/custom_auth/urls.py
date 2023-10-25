cf17c73396b9c74cc84906812b476041b62c7aae
Oleksandr <Brusiltsev1@gmail.com>
2023-10-25 13:24:39 +0300

    fix urls custom_auth

--- src/custom_auth/urls.py
+++ src/custom_auth/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from custom_auth.views import AuthRegisterView, TokenPairView
 
-urlpatterns = [
-    path("api/v1/register", AuthRegisterView.as_view(), name='auth_register'),
-    path('api/v1/login', TokenPairView.as_view(), name='auth_login'),
+urlpatterns=[
+    path("register",  AuthRegisterView.as_view(), name='auth_register' ),
+    path('login', TokenPairView.as_view(), name='auth_login'),
 
 ]
+
