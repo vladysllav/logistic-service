30165d05b23d54b5fc0bcd6d75fee932fac101a7
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-10-30 17:04:01 +0300

    add view for users

--- src/config/urls.py
+++ src/config/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("user_auth.urls")),
+    path("api/users/", include("users.urls")),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 
-# Create your views here.
+from .repositories import DatabaseUserRepository
+from .services import UserService
+
+
+class UsersListView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, requests):
+        user_service = UserService(DatabaseUserRepository())
+        users = user_service.get_users_list()
+        user_list = [
+            {
+                "user_id": user.id,
+                "user_type": user.user_type,
+                "email": user.email,
+                "fist_name": user.first_name,
+                "last_name": user.last_name,
+            }
+            for user in users
+        ]
+
+        return JsonResponse(user_list, status=status.HTTP_200_OK, safe=False)
