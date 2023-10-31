9e315a30b60e813a33d076970943c046626ef1c4
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-10-31 16:07:15 +0200

    add add serializers.py

--- src/users/views.py
+++ src/users/views.py
@@ -4,24 +4,15 @@ from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 from .repositories import DatabaseUserRepository
+from .serializers import UserListSerializer
 from .services import UserService
 
 
 class UsersListView(APIView):
     permission_classes = [AllowAny]
 
-    def get(self, requests):
+    def get(self, request):
         user_service = UserService(DatabaseUserRepository())
         users = user_service.get_users_list()
-        user_list = [
-            {
-                "user_id": user.id,
-                "user_type": user.user_type,
-                "email": user.email,
-                "fist_name": user.first_name,
-                "last_name": user.last_name,
-            }
-            for user in users
-        ]
-
+        user_list = UserListSerializer(users, many=True).data
         return JsonResponse(user_list, status=status.HTTP_200_OK, safe=False)
