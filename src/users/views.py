a896ee3fb0a5f266cf83c91e0174c608f423bd38
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-11-01 11:10:56 +0200

    fix serializers.py

--- src/users/views.py
+++ src/users/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 from .repositories import DatabaseUserRepository
-from .serializers import UserListSerializer
+from .serializers import UserSerializer
 from .services import UserService
 
 
@@ -14,5 +14,5 @@ class UsersListView(APIView):
     def get(self, request):
         user_service = UserService(DatabaseUserRepository())
         users = user_service.get_users_list()
-        user_list = UserListSerializer(users, many=True).data
+        user_list = UserSerializer(users, many=True).data
         return JsonResponse(user_list, status=status.HTTP_200_OK, safe=False)
