3270c9d1a9aeb82b68a4961ea35b45e099d41e81
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-11-01 11:22:49 +0200

    fix serializers.py

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -7,6 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
+            "id",
             "email",
             "password",
             "first_name",
