6151c9c02ecee3eee30e6e8755c7eb8b3bc06147
Gevorg <gevorg.ar.23@gmail.com>
2023-11-04 10:54:41 +0200

    permission add

--- src/warehouse/views.py
+++ src/warehouse/views.py
@@ -1,20 +1,21 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAdminUser
 
 from .models import Warehouse
+from .permissions import IsAdminOrReadOnly
 from .serializers import WarehouseSerializer
 
 
 class WarehouseAPIList(generics.ListCreateAPIView):
     queryset = Warehouse.objects.all()
     serializer_class = WarehouseSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class WarehouseAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Warehouse.objects.all()
     serializer_class = WarehouseSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class WarehouseAPIDestroyView(generics.RetrieveDestroyAPIView):
