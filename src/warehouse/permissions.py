c7175067767cae120cdb34f7abc62a5bbfca7dba
Gevorg <gevorg.ar.23@gmail.com>
2023-11-19 19:40:55 +0200

    update after review

--- src/warehouse/permissions.py
+++ src/warehouse/permissions.py
@@ -3,8 +3,9 @@ from rest_framework import permissions
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            if request.user.user_type == "admin":
-                return True
-            if request.method in permissions.SAFE_METHODS:
-                return True
+        if not request.user.is_authenticated:
+            return False
+        if request.user.user_type == "admin":
+            return True
+        if request.method in permissions.SAFE_METHODS:
+            return True
