31c078cd8b3161f3a96e853f5cc251e5d6dfea63
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-11-25 00:25:51 +0200

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -132,13 +132,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "postgres1",  
-        "USER": "postgres1",  
-        "PASSWORD": "postgres1",  
+        "NAME": os.environ.get("POSTGRES_DB", "postgres"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
         "HOST": "db",
         "PORT": "5432",
     }
-}
 
 CACHES = {
     "default": {
