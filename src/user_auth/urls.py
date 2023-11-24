0c66280c87d43fc761308f3ab647024c11b04f47
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-11-24 18:46:24 +0200

    Update urls.py

--- src/user_auth/urls.py
+++ src/user_auth/urls.py
@@ -5,5 +5,5 @@ from user_auth.views import AuthRegisterView, GoogleAuthView, LoginView
 urlpatterns = [
     path("register", AuthRegisterView.as_view(), name="auth_register"),
     path("login", LoginView.as_view(), name="auth_login"),
-    path("google-auth", GoogleAuthView.as_view(), name="google-auth"),
+    path("google-auth", GoogleAuthView.as_view(), name="google-auth"), 
 ]
